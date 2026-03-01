5b03d1d70e9e7cc40cc6d27fcb515c08cabb18b7
Shaik-Ishrath <167004394+Shaik-Ishrath@users.noreply.github.com>
2026-03-01 10:57:10 +0530

    Define basic operations in def.v

---
+++ def.v
@@ -0,0 +1,16 @@
+`define ADD 4'b0000
+`define INC 4'b0001
+`define SUB 4'b0010
+`define DEC 4'b0011
+`define MUL 4'b0100
+`define DIV 4'b0101
+`define SHL 4'b0110
+`define SHR 4'b0111
+`define INV 4'b1000
+`define AND 4'b1001
+`define OR 4'b1010
+`define NAND 4'b1011
+`define NOR 4'b1100
+`define XOR 4'b1101
+`define XNOR 4'b1110
+`define BUF 4'b1111
